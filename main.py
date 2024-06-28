1d2c5016bce11905dd66a4ce1dc3b43e19974ca9
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-28 09:04:25 -0700

    Fixed Pymongo Login And Updated Pyrogram Version

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from pyrogram import Client, idle
 from pyromod import listen  
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
+# Configure logging
 logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.INFO, 
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
 logging.getLogger("pymongo").setLevel(logging.ERROR)
 
+# Initialize start time
 StartTime = time.time()
+
+# Initialize the Client
 app = Client(
     "JARVIS",
     api_id=config.API_ID,
@@ -21,17 +26,24 @@ app = Client(
     plugins=dict(root="SESSIONGEN"),
 )
 
-
 if __name__ == "__main__":
     print("𝙹𝚊𝚛𝚟𝚒𝚜 𝚂𝚎𝚜𝚜𝚒𝚘𝚗 𝙶𝚎𝚗 𝚜𝚝𝚊𝚛𝚝𝚒𝚗𝚐...")
     try:
         app.start()
-    except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
+    except ApiIdInvalid:
+        raise Exception("Your API_ID is not valid.")
+    except ApiIdPublishedFlood:
+        raise Exception("Your API_ID/API_HASH is flood banned.")
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+        raise
+
     uname = app.get_me().username
     print(f"@{uname} NOW JARVIS SESSION GEN IS READY TO GEN SESSION")
+    
     idle()
+    
     app.stop()
     print("🇸 🇪 🇸 🇸 🇮 🇴 🇳  🇬 🇪 🇳 🇷 🇦 🇹 🇮 🇳 🇬  🇸 🇹 🇴 🇵 🇵 🇪 🇩...")
